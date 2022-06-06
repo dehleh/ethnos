9bf7299fb01bf7b8a2b8dff5358f1b96ea1ae478
dehleh <ogunladealaba@gmail.com>
2022-06-06 12:37:55 +0100

    server things

--- petscan/settings.py
+++ petscan/settings.py
@@ -143,3 +143,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    'https://ethnosvulnscan.vercel.app/login'
+]
