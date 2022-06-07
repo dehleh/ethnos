d9f355c03039aa8dd32c39064c07dd1626a0f245
dehleh <ogunladealaba@gmail.com>
2022-06-07 11:16:59 +0100

    edit

--- petscan/urls.py
+++ petscan/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('goodauth.urls')),
-    path('', include('ethaccount.urls'))
-    path('', include('ipurl.urls'))
+    path('', include('ethaccount.urls')),
+    path('', include('ipurl.urls')),
 ]
+
